8787549285cf52052ce0838929572dd5f48ec04b
MrLipa <57150296+MrLipa@users.noreply.github.com>
2022-05-03 12:35:53 +0200

    change

---
+++ Project2/Representation.py
@@ -0,0 +1,265 @@
+# pip install igraph
+# pip install pycairo
+# pip install cairocffi
+from igraph import *
+import copy
+
+data1={'name': 'Ad1.png',
+       'directed': True,
+       'colors': [],
+       'graph':{ 'a':  ['b','e','f'],
+                 'b':  ['a','c','f'],
+                 'c':  ['b','d','e','l'],
+                 'd':  ['c','h','i','k'],
+                 'e':  ['a','c','g','i'],
+                 'f':  ['a','b','g'],
+                 'g':  ['e','f','h'],
+                 'h':  ['d','g','i','l'],
+                 'i':  ['d','e','h','j'],
+                 'j':  ['i'],
+                 'k':  ['d'],
+                 'l':  ['c','h']}
+}
+
+data2={'name': 'Ad1.png',
+       'directed': True,
+       'colors': [],
+       'graph':{ 1:  [2,5,6],
+                 2:  [1,3,6],
+                 3:  [2,4,5,12],
+                 4:  [3,8,9,11],
+                 5:  [1,3,7,9],
+                 6:  [1,2,7],
+                 7:  [5,6,8],
+                 8:  [4,7,9,12],
+                 9:  [4,5,8,10],
+                 10: [9],
+                 11: [4],
+                 12: [3,8]}
+}
+
+####################AdjacencyList####################
+
+class AdjacencyList:
+    def __init__(self, data):
+        self.name=data['name']
+        self.directed=data['directed']
+        self.colors=data['colors']
+        self.graph=data['graph']
+
+        self.edges=[]
+
+        self.normalization={j: int(i) for i,j in enumerate(self.graph.keys())}
+
+        if self.directed:
+            for edge in self.graph.items():
+                self.edges.extend( [ (self.normalization[edge[0]],self.normalization[x]) for x in edge[1] if x>edge[0]] )
+        else:
+            for edge in self.graph.items():
+                self.edges.extend( [ (self.normalization[edge[0]],self.normalization[x]) for x in edge[1]] )
+
+    def printGraph(self):
+        print('Adjacency List ', end='\n\n')
+
+        for i,j in self.graph.items():
+            print( (str(i)+': ').ljust(5)  + str(j) )
+
+    def graphVisualization(self):
+        g = Graph(self.edges)
+
+        g.vs["label"] = list(self.graph.keys())
+        layout = g.layout_circle()
+
+        visual_style = {}
+        visual_style["vertex_size"] = 20
+        visual_style["layout"] = layout
+        visual_style["bbox"] = (800, 800)
+        visual_style["margin"] = 20
+
+        out=plot(g, **visual_style)
+        out.save(self.name)
+    def toAdjacencyList(self):
+        return self
+    def toAdjacencyMatrix(self):
+        data={}
+        data['name']=copy.deepcopy(self.name)
+        data['directed']=copy.deepcopy(self.directed)
+        data['colors']=copy.deepcopy(self.colors)
+
+        graph=[ [0 for _ in range(len(self.graph)) ] for _ in range(len(self.graph)) ]
+
+        for edge in self.edges:
+            graph[edge[0]][edge[1]] = 1
+            graph[edge[1]][edge[0]] = 1
+
+        data['graph']=copy.deepcopy(graph)
+
+        return AdjacencyMatrix(data)
+    def toIncidentMatrix(self):
+        data={}
+        data['name']=self.name
+        data['directed']=self.directed
+        data['colors']=self.colors
+
+        graph=[[0 for _ in range(len(self.edges))] for _ in range(len(self.graph)) ]
+
+        for i in range(len(self.edges)):
+            graph[self.edges[i][0]][i]=1
+            graph[self.edges[i][1]][i]=1
+
+        data['graph']=copy.deepcopy(graph)
+
+        return IncidentMatrix(data)
+
+####################AdjacencyMatrix####################
+
+class AdjacencyMatrix:
+    def __init__(self, data):
+        self.name=data['name']
+        self.directed=data['directed']
+        self.colors=data['colors']
+        self.graph=data['graph']
+
+        self.edges=[]
+
+        for i in range(len(self.graph)):
+            for j in range(i+1):
+                if self.graph[i][j]==1:
+                    self.edges.append( (i,j) )
+
+    def printGraph(self):
+        print('Adjacency Matrix ', end='\n\n')
+
+        for i in self.graph:
+            print(i)
+
+    def graphVisualization(self):
+        g = Graph(self.edges)
+
+        g.vs["label"] = list(range(len(self.graph)))
+        layout = g.layout_circle()
+
+        visual_style = {}
+        visual_style["vertex_size"] = 20
+        visual_style["layout"] = layout
+        visual_style["bbox"] = (800, 800)
+        visual_style["margin"] = 20
+
+        out=plot(g, **visual_style)
+        out.save(self.name)
+    def toAdjacencyList(self):
+        data={}
+        data['name']=copy.deepcopy(self.name)
+        data['directed']=copy.deepcopy(self.directed)
+        data['colors']=copy.deepcopy(self.colors)
+
+        graph={}
+
+        for i in range(len(self.graph)):
+            graph[i]=[]
+
+        for edge in self.edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+
+        data['graph']=copy.deepcopy(graph)
+
+        return AdjacencyList(data)
+    def toAdjacencyMatrix(self):
+        return self
+    def toIncidentMatrix(self):
+        data={}
+        data['name']=self.name
+        data['directed']=self.directed
+        data['colors']=self.colors
+
+        graph=[[0 for _ in range(len(self.edges))] for _ in range(len(self.graph)) ]
+
+        for i in range(len(self.edges)):
+            graph[self.edges[i][0]][i]=1
+            graph[self.edges[i][1]][i]=1
+
+        data['graph']=copy.deepcopy(graph)
+
+        return IncidentMatrix(data)
+
+####################IncidentMatrix####################
+
+class IncidentMatrix:
+    def __init__(self, data):
+        self.name=data['name']
+        self.directed=data['directed']
+        self.colors=data['colors']
+        self.graph=data['graph']
+
+        self.edges=[]
+
+        for i in range(len(self.graph[0])):
+            edge=[]
+            for j in range(len(self.graph)):
+                if self.graph[j][i]!=0 :
+                    edge.append(j)
+            self.edges.append(tuple(edge))
+
+    def printGraph(self):
+        print('Adjacency Matrix ', end='\n\n')
+
+        for i in self.graph:
+            print(i)
+
+    def graphVisualization(self):
+        g = Graph(self.edges)
+
+        g.vs["label"] = list(range(len(self.graph)))
+        layout = g.layout_circle()
+
+        visual_style = {}
+        visual_style["vertex_size"] = 20
+        visual_style["layout"] = layout
+        visual_style["bbox"] = (800, 800)
+        visual_style["margin"] = 20
+
+        out=plot(g, **visual_style)
+        out.save(self.name)
+    def toAdjacencyList(self):
+        data={}
+        data['name']=copy.deepcopy(self.name)
+        data['directed']=copy.deepcopy(self.directed)
+        data['colors']=copy.deepcopy(self.colors)
+
+        graph={}
+
+        for i in range(len(self.graph)):
+            graph[i]=[]
+
+        for edge in self.edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+
+        data['graph']=copy.deepcopy(graph)
+
+        return AdjacencyList(data)
+    def toAdjacencyMatrix(self):
+        return self
+    def toIncidentMatrix(self):
+        data={}
+        data['name']=self.name
+        data['directed']=self.directed
+        data['colors']=self.colors
+
+        graph=[[0 for _ in range(len(self.edges))] for _ in range(len(self.graph)) ]
+
+        for i in range(len(self.edges)):
+            graph[self.edges[i][0]][i]=1
+            graph[self.edges[i][1]][i]=1
+
+        data['graph']=copy.deepcopy(graph)
+
+        return IncidentMatrix(data)
+
+
+# a=AdjacencyList(data1)
+# a.toAdjacencyMatrix().toAdjacencyList().graphVisualization()
+# a.toAdjacencyMatrix().toIncidentMatrix().graphVisualization()
+# a.toIncidentMatrix().toAdjacencyList().graphVisualization()
+# a.toIncidentMatrix().toAdjacencyMatrix().graphVisualization()
